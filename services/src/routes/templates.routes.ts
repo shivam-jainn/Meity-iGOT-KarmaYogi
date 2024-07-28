700546a80d41c508437a3cab18d015bfd1efe775
shivam-jainn <shivam.clgstash@gmail.com>
2024-07-28 12:51:42 +0530

    feat: :sparkles: created nocode backend
    nada
    
    nada

--- services/src/routes/templates.routes.ts
+++ services/src/routes/templates.routes.ts
@@ -1,6 +1,8 @@
 import { PrismaClient, Prisma } from '@prisma/client';
 import express, { Request, Response } from 'express';
-
+import cors from 'cors';
+import { uploadFile,downloadFile } from '../utils/s3.utils';
+import 'dotenv/config';
 const prisma = new PrismaClient();
 const templateRouter = express.Router();
 
@@ -58,5 +60,30 @@ templateRouter.get('/list/sms', async (req: Request, res: Response) => {
     }
 });
 
+
+templateRouter.post('/upload/nocode'
+  ,async (req: Request, res: Response) => {
+  try {
+    const {json} = req.body;
+    const templateName = `${Date.now()}.json`;
+    const result = await uploadFile(Buffer.from(json), templateName, process.env.AWS_TEMP_BUCKET as string);
+    console.log(result);
+    return res.json({json:json});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while uploading template' });
+  }
+})
+
+templateRouter.get('/download/nocode/:templateName', async (req: Request, res: Response) => {
+  try {
+    const {templateName} = req.params;
+    const template = await downloadFile(process.env.AWS_TEMP_BUCKET as string, templateName);
+    return res.json(template);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while downloading template' });
+  }
+});
 
 export default templateRouter;
